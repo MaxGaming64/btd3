6ca4a834b36d5305dcf40b99f41ad3f7d09557d5
MaxPlayer2011 <maximus.naumus@gmail.com>
2022-05-07 19:03:37 -0400

    finish finale outro

---
+++ Assets/Scripts/GC_Outro.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GC_Outro : MonoBehaviour
+{
+    public bool paused;
+    public GameObject pauseCanvas;
+    private AudioSource mainMus;
+    public DialogueSystem ds;
+    public AudioClip ambient;
+
+    void Start()
+    {
+        if (GameObject.FindGameObjectWithTag("MainMus") == null)
+        {
+            mainMus = new GameObject().AddComponent<AudioSource>();
+            mainMus.gameObject.name = "MainMus";
+            mainMus.gameObject.tag = "MainMus";
+            mainMus.loop = true;
+            mainMus.clip = ambient;
+            mainMus.Play();
+            DontDestroyOnLoad(mainMus);
+        }
+
+        else
+        {
+            mainMus = GameObject.FindGameObjectWithTag("MainMus").GetComponent<AudioSource>();
+            mainMus.loop = true;
+
+            if (mainMus.clip != ambient)
+            {
+                mainMus.clip = ambient;
+                mainMus.Play();
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause") & !ds.dialogue)
+        {
+            if (paused)
+            {
+                paused = false;
+                Time.timeScale = 1f;
+                pauseCanvas.SetActive(false);
+            }
+
+            else
+            {
+                paused = true;
+                Time.timeScale = 0f;
+                pauseCanvas.SetActive(true);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q) & paused)
+        {
+            Time.timeScale = 1f;
+            Destroy(mainMus.gameObject);
+            SceneManager.LoadScene("MenuOutro");
+        }
+    }
+}
