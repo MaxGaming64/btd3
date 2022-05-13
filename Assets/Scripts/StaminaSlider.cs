1c199d82e6ebf25abdb63be84fc60f523d0f02d2
MaxPlayer2011 <maximus.naumus@gmail.com>
2022-05-13 07:53:09 -0400

    refill stamina when player is disabled

---
+++ Assets/Scripts/StaminaSlider.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaSlider : MonoBehaviour
+{
+    private Player player;
+    
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    void Update()
+    {
+        if (!player.enabled | !player.gameObject.activeSelf)
+        {
+            GetComponent<Slider>().value += 20f * Time.deltaTime;
+        }
+    }
+}
