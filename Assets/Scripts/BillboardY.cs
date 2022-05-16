6996107cfaa13a446ba6670805f5b529e82b1984
MaxPlayer2011 <maximus.naumus@gmail.com>
2022-05-16 18:57:47 -0400

    fix up billboard y

--- Assets/Scripts/BillboardY.cs
+++ Assets/Scripts/BillboardY.cs
@@ -4,10 +4,6 @@ public class BillboardY : MonoBehaviour
 {
 	void LateUpdate()
 	{
-		transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward);
-
-		Vector3 eulerAngles = transform.eulerAngles;
-		eulerAngles.x = 0f;
-		transform.eulerAngles = eulerAngles;
+		transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
 	}
 }
