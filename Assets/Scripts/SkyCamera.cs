156d47af1d45fc0821d52e37d39ae86708bc4f96
MaxPlayer2011 <maximus.naumus@gmail.com>
2022-05-20 17:59:26 -0400

    improve skycamera

--- Assets/Scripts/SkyCamera.cs
+++ Assets/Scripts/SkyCamera.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class SkyCamera : MonoBehaviour
 {
-    public float moveSpeed;
+    public float skyScale;
     
     void LateUpdate()
     {
         transform.rotation = Camera.main.transform.rotation;
-        transform.localPosition = Camera.main.transform.position / moveSpeed;
+        transform.localPosition = Camera.main.transform.position / skyScale;
     }
 }
