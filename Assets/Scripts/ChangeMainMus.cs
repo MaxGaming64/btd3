8b0aa2a035abcf33e0d9dea00c69d851f45fd24c
MaxPlayer2011 <maximus.naumus@gmail.com>
2022-07-05 12:47:01 -0400

    add seamless option to changemainmus, with quick showcase

--- Assets/Scripts/ChangeMainMus.cs
+++ Assets/Scripts/ChangeMainMus.cs
@@ -5,6 +5,7 @@ public class ChangeMainMus : MonoBehaviour
     public AudioClip clip;
     public float volume;
     public float pitch;
+    public bool seamless;
     
     private void OnTriggerEnter(Collider other)
     {
@@ -14,10 +15,22 @@ public class ChangeMainMus : MonoBehaviour
 
             if (audioSource.clip != clip)
             {
+                float num = audioSource.time;
+
                 audioSource.clip = clip;
                 audioSource.volume = volume;
                 audioSource.pitch = pitch;
                 audioSource.Play();
+
+                if (seamless)
+                {
+                    audioSource.time = num;
+                }
+
+                else
+                {
+                    audioSource.time = 0f;
+                }
             }
         }
     }
