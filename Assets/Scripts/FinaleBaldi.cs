9547b24065d1e8c4c2617d691921bc345f2ee6e2
MaxPlayer2011 <maximus.naumus@gmail.com>
2022-07-05 19:27:22 -0400

    Update FinaleBaldi.cs

--- Assets/Scripts/FinaleBaldi.cs
+++ Assets/Scripts/FinaleBaldi.cs
@@ -4,7 +4,7 @@ public class FinaleBaldi : MonoBehaviour
 {
     public Transform player;
 
-    void Update()
+    void LateUpdate()
     {
         transform.position = player.position - Vector3.forward * 5f;
     }
